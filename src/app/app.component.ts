4b6eca31857e7d4e51dd911b318b380c5ad96666
Kevin <kevincosta13679@outlook.com>
2021-01-15 12:48:32 -0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-escribo-inovacao';
+  result = 0;
+
+  SomeValues(value: any){
+    if (Number(value) > 0) {
+
+      let soma3 : number = 0;
+      let soma5 : number = 0;
+
+      for (let index = 1; index < (value/3); index++) {
+        soma3 += 3 * index;
+
+      }
+      for (let index = 1; index < (value/5); index++) {
+        soma5 +=  5 * index;
+
+      }
+      console.log(soma3);
+      console.log(soma5);
+
+      console.log(soma3 + soma5);
+      this.result = (soma3 + soma5);
+    }
+  }
 }
